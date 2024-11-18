0019cdec7a13651a8bad25a12ba67144590d0305
Jono-Hills <jonathan.hills@sleeq.global>
2024-11-18 19:18:50 +1000

    Move names to external datafile

--- secretSanta.rb
+++ secretSanta.rb
@@ -1,18 +1,11 @@
 require 'fileutils'
+require 'json'
 
-# Define the outputs folder path
+# File constants
 output_folder = 'outputs'
+input_path = 'inputs/names.json'
 
-# Name => [ignored pairings]
-names = {
-    'Jono' => ['Roosje'],
-    'Roosje' => ['Jono'],
-    'Jasmyn' => ['Chalisa'],
-    'Chalisa' => ['Jasmyn'],
-    'Phet' => [],
-    'Hailey' => ['Dylan'],
-    'Dylan' => ['Hailey']
-}
+names = JSON.parse(File.read(input_path))
 
 recipients = Array.new(names.keys)
 senders = Array.new(names.keys)
@@ -25,8 +18,6 @@ FileUtils.mkdir_p(output_folder)
 for sender in senders do
   index = rand(recipients.length)
   recipient = recipients[index]
-  while names[sender].include?(recipient) || recipient == sender do
-    puts "#{sender} => #{recipient}"
     if recipients.size == 1
       raise RuntimeError.new("Loop detected - please try again")
     end
@@ -34,7 +25,6 @@ for sender in senders do
     index = rand(recipients.length)
     recipient = recipients[index]
   end
-
   # Write the file to the outputs folder
   file_path = File.join(output_folder, "#{sender}.txt")
   File.open(file_path, 'w') { |file| file.write(recipient) }
